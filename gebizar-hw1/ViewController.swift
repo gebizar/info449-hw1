4100fca61b2ad4077341b7eb331e40b9d29b74c6
Gabe <gebizar@uw.edu>
2019-01-10 14:39:20 -0800

    Completed hw1 to the best of my ability

---
+++ gebizar-hw1/ViewController.swift
@@ -0,0 +1,21 @@
+//
+//  ViewController.swift
+//  gebizar-hw1
+//
+//  Created by Gabe Bizar on 1/10/19.
+//  Copyright © 2019 Gabe Bizar. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        print("Go Seahawks!")
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
---
+++ gebizar-hw1Tests/gebizar_hw1Tests.swift
@@ -0,0 +1,34 @@
+//
+//  gebizar_hw1Tests.swift
+//  gebizar-hw1Tests
+//
+//  Created by Gabe Bizar on 1/10/19.
+//  Copyright © 2019 Gabe Bizar. All rights reserved.
+//
+
+import XCTest
+@testable import gebizar_hw1
+
+class gebizar_hw1Tests: XCTestCase {
+
+    override func setUp() {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDown() {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
